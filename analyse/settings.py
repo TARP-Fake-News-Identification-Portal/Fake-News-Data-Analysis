4cf03fefa899fd2ff35d48033a2835735cea4347
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2022-07-30 20:55:35 +0530

    Update requirements and cleanup API call

--- analyse/settings.py
+++ analyse/settings.py
@@ -2,9 +2,8 @@ from dotenv import load_dotenv, find_dotenv
 import os
 
 load_dotenv(find_dotenv())
-API_KEY = os.environ['API_KEY']
-API_KEY_SECRET = os.environ['API_KEY_SECRET']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+API_KEY = os.environ["API_KEY"]
+API_KEY_SECRET = os.environ["API_KEY_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 SESSION_COOKIE_SECURE = True
-SECRET_KEY = os.environ['SECRET_KEY']
